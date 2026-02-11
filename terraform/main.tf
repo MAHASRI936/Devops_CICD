180ba3c03cdc6c9a111480be62d6a327a6388c3b
MahasriSaravanan <mahasris.23csd@kongu.edu>
2026-02-11 14:40:27 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "game_server" {
-  ami           = "ami-019715e0d74f695be" 
+  ami           = "ami-019715e0d74f695be"
   instance_type = "t3.small"
   key_name      = "dev"
 
